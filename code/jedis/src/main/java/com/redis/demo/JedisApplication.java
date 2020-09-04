18c32aef5827b6f74b80deb2a08dc1214b9d7da9
sourlemon <work_Xq@163.com>
2020-09-04 14:33:04 +0800

    project structure  update

---
+++ code/jedis/src/main/java/com/redis/demo/JedisApplication.java
@@ -0,0 +1,14 @@
+package com.redis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Jedis 集成redis
+ */
+@SpringBootApplication
+public class JedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JedisApplication.class);
+    }
+}
---
+++ code/jedisson/src/main/java/com/redis/demo/JedissonApplication.java
@@ -0,0 +1,18 @@
+package com.redis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 内置Redis
+ *
+ * @Author: ShayLau
+ * @Date: 2020/8/28 15:23
+ */
+@SpringBootApplication
+public class JedissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JedissonApplication.class);
+    }
+}
